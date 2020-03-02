fa087cb2190ef09cde9bab6cf35300ef55bab98e
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-02 21:50:43 +0800

    read string input func

--- backend/solver.js
+++ backend/solver.js
@@ -1,3 +1,15 @@
-function solveBoggle(){
-    
+function solveBoggle(charArrayInput){
+    //must ensure that charArray has exactly 16 elements
+    var charArray = charArrayInput.split(", ")
+    var arr = []
+    for (i = 0; i < 4; i++){
+        var arr_element = []
+        for (j = 0; j < 4; j++){
+            arr_element.push(charArray[4*i + j])
+        }
+        arr.push(arr_element)
+    }
+    console.log(arr)
 }
+
+solveBoggle("A, C, E, D, L, U, G, *, E, *, H, T, G, A, F, K")
