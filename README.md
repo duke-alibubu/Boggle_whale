13951ccc64235e337ef0508fcacbd27e0461bc43
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-04 01:41:33 +0800

    add MySQL databases

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Boggle_whale
+
+## Running Databases
+- In MySQL command shell, type `source D:\My Folder\Saleswhale_Chall\Boggle_whale\backend\database\schema.sql`
---
+++ backend/database/schema.sql
@@ -0,0 +1,14 @@
+USE boggle;
+
+DROP TABLE IF EXISTS game;
+
+CREATE TABLE game
+(
+    id  INT unsigned NOT NULL AUTO_INCREMENT,
+    token VARCHAR(150) NOT NULL,
+    board VARCHAR(150) NOT NULL,
+    duration INT unsigned NOT NULL,
+    points INT unsigned NOT NULL,
+    time_left INT unsigned NOT NULL,
+    PRIMARY KEY (id)
+);
