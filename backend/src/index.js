da1340aafd1f4d7d0886f1b221d4cf50f4a244df
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-03 16:02:56 +0800

    init npm project backend

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const port = process.env.port
+const app = express()
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
