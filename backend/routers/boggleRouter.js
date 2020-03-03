9d54fe5bd672a70603f0e3a3bcb51e34a25da044
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-03 16:15:06 +0800

    template for backend router

---
+++ backend/routers/boggleRouter.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = new express.Router()
+
+router.post('/games', async (req, res) => {
+    try {
+
+    }
+    catch (e){
+
+    }
+})
+
+router.put('/games/:id', async (req, res) => {
+    try {
+
+    }
+    catch (e){
+        
+    }
+})
+
+router.get('/games/:id', async (req, res) => {
+    try {
+
+    }
+    catch (e){
+        
+    }
+})
+
+module.exports = router
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const port = process.env.port
 const app = express()
+const boggleRouter = require('../routers/boggleRouter')
 
 app.use(express.json())
+app.use(boggleRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
