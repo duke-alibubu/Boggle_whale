5f9b54d95622796812ea067734a5bf87dec04d3b
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-06 15:22:14 +0800

    random input ok

--- frontend/src/javascript/GameBoard.js
+++ frontend/src/javascript/GameBoard.js
@@ -12,23 +12,23 @@ export class GameBoard extends React.Component {
         return (
             <div >
                 <div id="board-content">
-                    <div class="game-board">
-                        <div class="game-box">{this.state.board_array[0]}</div>
-                        <div class="game-box">{this.state.board_array[1]}</div>
-                        <div class="game-box">{this.state.board_array[2]}</div>
-                        <div class="game-box">{this.state.board_array[3]}</div>
-                        <div class="game-box">{this.state.board_array[4]}</div>
-                        <div class="game-box">{this.state.board_array[5]}</div>
-                        <div class="game-box">{this.state.board_array[6]}</div>
-                        <div class="game-box">{this.state.board_array[7]}</div>
-                        <div class="game-box">{this.state.board_array[8]}</div>
-                        <div class="game-box">{this.state.board_array[9]}</div>
-                        <div class="game-box">{this.state.board_array[10]}</div>
-                        <div class="game-box">{this.state.board_array[11]}</div>
-                        <div class="game-box">{this.state.board_array[12]}</div>
-                        <div class="game-box">{this.state.board_array[13]}</div>
-                        <div class="game-box">{this.state.board_array[14]}</div>
-                        <div class="game-box">{this.state.board_array[15]}</div>
+                    <div className="game-board">
+                        <div className="game-box">{this.state.board_array[0]}</div>
+                        <div className="game-box">{this.state.board_array[1]}</div>
+                        <div className="game-box">{this.state.board_array[2]}</div>
+                        <div className="game-box">{this.state.board_array[3]}</div>
+                        <div className="game-box">{this.state.board_array[4]}</div>
+                        <div className="game-box">{this.state.board_array[5]}</div>
+                        <div className="game-box">{this.state.board_array[6]}</div>
+                        <div className="game-box">{this.state.board_array[7]}</div>
+                        <div className="game-box">{this.state.board_array[8]}</div>
+                        <div className="game-box">{this.state.board_array[9]}</div>
+                        <div className="game-box">{this.state.board_array[10]}</div>
+                        <div className="game-box">{this.state.board_array[11]}</div>
+                        <div className="game-box">{this.state.board_array[12]}</div>
+                        <div className="game-box">{this.state.board_array[13]}</div>
+                        <div className="game-box">{this.state.board_array[14]}</div>
+                        <div className="game-box">{this.state.board_array[15]}</div>
                     </div>
                 </div>
                 <InputTable />
--- frontend/src/javascript/InputTable.js
+++ frontend/src/javascript/InputTable.js
@@ -1,38 +1,57 @@
 import React from 'react';
 
 export class InputTable extends React.Component {
+    constructor(props) {
+        super(props)
+    
+        this.random = true
+        this.loadInput = this.loadInput.bind(this)
+        this.randomChange = this.randomChange.bind(this)
+    }
+
+    loadInput(){
+        console.log(this.duration.value, this.random)
+    }
+
+    randomChange(e){
+        this.random = !this.random
+    }
     render() {
         return (
             <div id="input-table">
                 <table>
                     <tr>
                         <td>
-                            <label for="game_duration">Duration: </label>
-                            <input type="number" id="game_duration" name="game_duration" value="180000" />
+                            <label htmlFor="game_duration">Duration: </label>
+                            <input type="number" id="game_duration" ref={(dur) => this.duration=dur}  name="game_duration" value="180000" />
                             <span>ms</span>
-                            <br />
+                            <br/>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <span>Random Board? </span>
-                            <div class="radio">
-                                <label><input type="radio" name="rd_yes" checked />Yes</label>
+                            {/* <div class="radio">
+                                <label><input type="radio" name="randomize" ref={(a) => this.random=true} checked />Yes</label>
                             </div>
                             <div class="radio">
-                                <label><input type="radio" name="rd_no" />No</label>
-                            </div>
+                                <label><input type="radio" name="randomize" ref={(b) => this.random=false} />No</label>
+                            </div> */}
+                            <select onChange={this.randomChange} >
+                                <option value="A">Yes</option>
+                                <option value="B">No</option>
+                            </select>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <label for="board_input">Board: </label>
+                            <label htmlFor="board_input">Board: </label>
                             <textarea id="board_input" name="board_input" value="T, A, P, *, E, A, K, S, O, B, R, S, S, *, X, D" />
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <button id="load_input" onclick="">Load Input</button>
+                            <button id="load_input" onClick={this.loadInput}>Load Input</button>
                         </td>
                     </tr>
                 </table>
